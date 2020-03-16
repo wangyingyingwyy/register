3bcab518376b423e5877517688baa127d098d310
wangyingyingwyy <2423457156@qq.com>
2020-03-16 20:09:16 +0800

    提交

---
+++ Gruntfiles.js
@@ -0,0 +1,52 @@
+module.exports=function (grunt) {  
+    grunt.initConfig({
+        htmlmin:{
+            options:{
+                collapseWhitespace: true,
+                preserveLineBreaks: true
+            },
+            files:{
+                src:'./index.html',
+                dest:'dist/index.html'
+            }
+        },
+        cssmin:{
+            options: {
+                mergeIntoShorthands: false,
+                roundingPrecision: -1
+              },
+              target: {
+                files: {
+                  'dist/reg.min.css': ['./reg.css']
+                }
+              }
+        },
+        uglify:{
+            'dist/reg.min.js':'./reg.js'
+        },
+        useminPrepare: {
+            html: 'index.html',
+            options: {
+              dest: 'dist'
+            }
+        },
+        usemin: {
+            html: ['dist/index.html']
+        },
+        clean: ['dist/reg.js'],
+        copy: {
+          html: {
+            src: './index.html',
+            dest: './dist/index.html'
+          }
+        }
+      
+    });
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-usemin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.registerTask('release', ['copy', 'useminPrepare', 'concat', 'uglify', 'usemin', 'cssmin', 'htmlmin', 'clean']);
+}
